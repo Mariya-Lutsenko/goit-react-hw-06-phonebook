90e3d4d8565e70af0750d7db8f4d516c78e6f9f6
Mariya-Lutsenko <107764269+Mariya-Lutsenko@users.noreply.github.com>
2023-02-23 12:51:35 +0200

    Redux tollkit basic

--- src/redux/contacts/contacts-reducer.js
+++ src/redux/contacts/contacts-reducer.js
@@ -2,10 +2,10 @@ import { createReducer } from '@reduxjs/toolkit';
 import { addContact, removeContact } from './contacts-actions';
 
 const contactsReducer = createReducer([], {
-  // Можна записувати [addContact.type]
   [addContact]: (store, action) => [...store, action.payload],
   [removeContact]: (store, action) =>
     store.filter(({ id }) => id !== action.payload),
 });
 
 export default contactsReducer;
+// Можна записувати [addContact.type]
